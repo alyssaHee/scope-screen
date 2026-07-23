7a0c863dbfb5233bbaa75301a37a98bf16946118
Alyssa Hee <alyssahee43@gmail.com>
2026-07-23 14:43:25 -0500

    Add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import {
   Routes,
   Route,
 } from 'react-router-dom'
-
+import { useEffect } from 'react';
+import { inject } from '@vercel/analytics';
 import Home from './pages/Home'
 import Experience from './pages/Experience'
 import Projects from './pages/Projects'
 import Misc from './pages/Misc'
 
 function App() {
+useEffect(() => {
+    inject();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes key={location.pathname}>
